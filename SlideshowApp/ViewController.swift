4ca6dc42062a39253347beb14902798842f84261
daigo <h.k.tamers@hotmail.co.jp>
2020-06-21 18:30:22 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 小林大悟 on 2020/06/21.
+//  Copyright © 2020 daigo.kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
